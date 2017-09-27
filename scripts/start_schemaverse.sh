c423446b32edfe4a2580c0073551354eac1b04e3
frozenfoxx <siliconfoxx@gmail.com>
2017-09-26 19:08:42 -0500

    Added an init script

---
+++ scripts/start_schemaverse.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+####################################################
+# Author: FrozenFOXX
+# Description: this script launches the Schemaverse
+####################################################
+
+# Variables
+PORT=5432
+
+# Logic
+
+## Start Database
+/etc/init.d/postgresql start
