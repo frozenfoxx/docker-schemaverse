7df65d58492ba0786f40e9973bdcb0846b3000a2
frozenfoxx <siliconfoxx@gmail.com>
2017-09-28 20:59:09 -0500

    Added server tic script

--- scripts/start_schemaverse.sh
+++ scripts/start_schemaverse.sh
@@ -12,5 +12,8 @@ PORT=5432
 ## Start database
 /etc/init.d/postgresql start
 
+## Start tic.pl
+/src/schemaverse/tic.pl > /src/schemaverse/tic.log 2>&1 &
+
 ## Watch log
 tail -f /var/log/postgresql/postgresql-9.5-main.log
