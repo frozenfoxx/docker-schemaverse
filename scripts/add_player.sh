58cec5e2a212008399bebabbe020a84356913580
frozenfoxx <siliconfoxx@gmail.com>
2017-10-04 17:27:57 -0500

    Upadded add_player.sh

--- scripts/add_player.sh
+++ scripts/add_player.sh
@@ -7,11 +7,15 @@
 # Variables
 PLAYERNAME=${1}
 PASSWORD=${2}
+BALANCE=${3:-10000}
+FUELRESERVE=${4:-100000}
 
 # Logic
 if [ $# -lt 2 ]; then
   echo "Not enough arguments"
-  echo "Usage: add_player.sh USERNAME PASSWORD"
+  echo "Usage: add_player.sh [USERNAME] [PASSWORD] [BALANCE] [FUEL RESERVE]"
+  echo "  Note: only username and password are required"
 fi
 
-su schemaverse -c "psql -d schemaverse -c \"INSERT INTO player(username, password) VALUES('${PLAYERNAME}', '${PASSWORD}');\"
+su schemaverse -c "psql -d schemaverse -c \"INSERT INTO player(username, password, balance, fuel_reserve) \
+  VALUES('${PLAYERNAME}', 'md5'+MD5(${PASSWORD}+${PLAYERNAME}), ${BALANCE}, ${FUELRESERVE});\"
